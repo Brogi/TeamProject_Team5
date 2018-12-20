b5b51e28bdb27f8253030c52c8f4e76f0248eb29
jw <ljw8161@naver.com>
2018-12-20 15:02:42 +0900

    interface frame & Introduce Menu

--- temp.c
+++ temp.c
@@ -1,54 +1,54 @@
-
+//박상면 시스템함수구현
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
 
-#define MAX_horizon 30
-#define MAX_vertical 20
+#define MAX_horizon 50
+#define MAX_vertical 30
 
 //checking player
 //making object -> using random function
 //get signal
 
 void temp_moving(){
+	//방향키 입력받는 함수사용
 		int ch;
 
 		while((ch=getch()) !=KEY_F(1)){
-		clear();
-        addstr("Check line : ");
-
+        addstr("              ");
+	
         switch(ch){
             case KEY_LEFT:
-                move(1,15);
-                addstr(" Key Left");
-                break;
+                move(2,MAX_vertical/2);
+                addstr("Key Left");
+		break;
        		case KEY_RIGHT:
-                move(1,15);
-                addstr(" Key Right");
+                move(2,MAX_vertical/2);
+                addstr("Key Right");
                 break;
          	case KEY_UP:
-                move(1,15);
-                addstr(" Key Up");
+                move(2,MAX_vertical/2);
+                addstr("Key Up");
                 break;
          	case KEY_DOWN:
-                move(1,15);
-                addstr(" Key Down");
+                move(2,MAX_vertical/2);
+                addstr("Key Down");
                 break;
-        	case KEY_F(1):
-                addstr("Introduce Game Rule");
-                break;
+        	
         	refresh();
         	}
-		 }
+	 }
 }
 
 void field(){
+	//게임 필드 구현
+	//가로 화면크기  MAX_horizon, 세로화면크기 MAX_vertical
 	int i,j;
-
+//	clear();
 	for(i=0;i<MAX_vertical;i++){
 		if(i==0){
-			for(j=0;j<MAX_horizon;j++)
+			for(j=0;j<MAX_horizon+1;j++)
 			addstr("*");
 			continue;
 		}
@@ -66,10 +66,8 @@ void field(){
 			continue;
 		}
 	}
-
-
-
-
+	move(1,MAX_horizon/3);
+	addstr("Press F1 to exit");
 }
 
 
@@ -77,6 +75,7 @@ int main( int argc, char *argv[]){
 
 
 	initscr();
+	//초기화면
 	field();
 	cbreak();
 	keypad(stdscr, TRUE);
